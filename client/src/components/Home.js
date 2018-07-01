f260dc1be8531d256c0ec8825af1b70e4881d6b7
Alan Bischoff <alanlbischoff@gmail.com>
2018-07-01 15:05:25 -0600

    home header and navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { 
+  Container,
+  Header,
+  Image,
+} from 'semantic-ui-react';
+import styled from 'styled-components'
+import NavBar from './NavBar';
 
 class Home extends Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+<Container>
+      <HeaderImage>
+        <NavContainer>
+        <NavBar />
+        </NavContainer>
+        <HeadContainer>
+           <Header as='h1'  color="inverted">Authentic Adventure</Header>
+           </HeadContainer>
+      </HeaderImage>
+  </Container>
     );
   }
 }
+
+const HeaderImage = styled.div`
+  background-image: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('https://images.unsplash.com/photo-1488441770602-aed21fc49bd5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f093285a418b6aadfc00bd4b8354ec02&auto=format&fit=crop&w=1350&q=80');
+  background-position: center;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 40vh;
+  margin-bottom: 30px;
+  color: white;
+`
+const HeadContainer = styled.div`
+display: flex;
+align-self: flex-end;
+justify-content: center;
+`
+const NavContainer = styled.div`
+display: flex;
+align-items: flex-start;
+justify-content: center;
+flex: space-around;
+color: inverted;
+`
+
+
 
 export default Home;
